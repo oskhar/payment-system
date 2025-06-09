3d98689bd17c450199d7c1657ab13f5a798d6f89
oskhar <muhamadoskharm@gmail.com>
2025-06-09 13:15:46 +0700

    🛡 security(deployment): enable cors for production

--- api-app/src/main.ts
+++ api-app/src/main.ts
@@ -4,6 +4,7 @@ import { ErrorFilter } from './common/filter/error.filter';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 import * as express from 'express';
 import { join } from 'path';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -14,6 +15,9 @@ async function bootstrap() {
     '/uploads/image',
     express.static(join(process.cwd(), 'uploads/image')),
   );
+
+  app.enableCors();
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   // await app.listen(process.env.PORT ?? 3000, '192.168.1.12');
   // await app.listen(process.env.PORT ?? 3000, '192.168.43.147');
